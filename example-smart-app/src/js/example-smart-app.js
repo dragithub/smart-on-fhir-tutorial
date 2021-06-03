738fe17cff6990dd07e781c75317c164a4024506
dragithub <emldra@yahoo.co.uk>
2021-06-03 14:11:25 +0100

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -18,8 +18,8 @@
                         $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
                               'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
                               'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
-                      },
-					  date: 'gt 2020-01-01';
+                      }/*,
+					  date: 'gt 2020-01-01';*/
 					  category: 'vital-signs'
                     }
                   });
