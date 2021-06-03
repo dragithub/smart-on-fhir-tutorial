a923616ae366d4b25748382926041df2eba4cb7d
dragithub <emldra@yahoo.co.uk>
2021-06-03 14:25:26 +0100

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -19,9 +19,12 @@
                               'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
                               'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
                       }
+                      },  
+					  category: 'vital-signs'
 
                     }
                   });
+/*, date: 'gt 2020-01-01';*/
 
         $.when(pt, obv).fail(onError);
 
